1026d4b16eb2a629d3c2f8886ee3a01df618a245
Onuorah Ugochukwu <ugolymail@gmail.com>
2018-08-26 22:46:17 +0100

    update-features added updated db.js file to include votes table

--- db.js
+++ db.js
@@ -15,24 +15,30 @@ function runQuery(querystring, data = []) {
 
 function userTB() {
   let u =
-    "CREATE TABLE users(id SERIAL PRIMARY KEY, name VARCHAR(50) NOT NULL, email VARCHAR(50) " +
-    "NOT NULL UNIQUE, password VARCHAR(100) NOT NULL)";
+    "CREATE TABLE votes (id SERIAL NOT NULL, vote integer NOT NULL, answerid integer NOT NULL, " +
+    "votedate DATE DEFAULT CURRENT_DATE, votedby integer NOT NULL )";
   runQuery(u)
     .then(result => {
       u =
-        "CREATE TABLE questions(id SERIAL PRIMARY KEY, title VARCHAR(80) NOT NULL, body TEXT NOT NULL, askedby INTEGER NOT NULL, " +
-        "askedate DATE DEFAULT CURRENT_DATE)";
-      runQuery(u).then(qns => {
+        "CREATE TABLE users(id SERIAL PRIMARY KEY, name VARCHAR(50) NOT NULL, email VARCHAR(50) " +
+        "NOT NULL UNIQUE, password VARCHAR(100) NOT NULL)";
+      runQuery(u).then(result => {
         u =
-          "CREATE TABLE answers(id SERIAL PRIMARY KEY, body TEXT NOT NULL, answeredby INTEGER NOT NULL, " +
-          "ansdate DATE DEFAULT CURRENT_DATE, questionid INTEGER NOT NULL)";
-        runQuery(u).then(res => {
+          "CREATE TABLE questions(id SERIAL PRIMARY KEY, title VARCHAR(80) NOT NULL, body TEXT NOT NULL, askedby INTEGER NOT NULL, " +
+          "askedate DATE DEFAULT CURRENT_DATE)";
+        runQuery(u).then(qns => {
           u =
-            "CREATE TABLE public.votes (id SERIAL NOT NULL, vote integer NOT NULL, answerid integer NOT NULL, " +
-            "votedate DATE DEFAULT CURRENT_DATE, votedby integer NOT NULL )";
+            "CREATE TABLE answers(id SERIAL PRIMARY KEY, body TEXT NOT NULL, answeredby INTEGER NOT NULL, " +
+            "ansdate DATE DEFAULT CURRENT_DATE, questionid INTEGER NOT NULL)";
+          runQuery(u).then(res => {
+            u =
+              "CREATE TABLE votes (id SERIAL NOT NULL, vote integer NOT NULL, answerid integer NOT NULL, " +
+              "votedate DATE DEFAULT CURRENT_DATE, votedby integer NOT NULL )";
+          });
         });
       });
     })
+
     .catch(err => {
       //console.log(err);
     });
